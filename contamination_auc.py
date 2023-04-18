Fixed normalization, added time difference, added TODO, general cleanup.

contamination_auc.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pandas as pd
 import PID_with_Plotting
+import tqdm
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 
 BIT_CONVERSION_FACTOR = 4.8828
 
+# TODO Smooth Data -> Moving Window 5-10
+# TODO Rising Side (Do everything in reverse) Go to 95% of max
+# TODO Save Normalization Figures
+# TODO Make each odor-concentration pair the same color
+# TODO Progress Bar (trange)
 
 def get_auc(x, y):
     trap = np.trapz(y, x)
@@ -13,13 +18,8 @@ def get_auc(x, y):
 
 
 def normalize_data(sniff_data):
-    #scaler = preprocessing.MinMaxScaler()
-    #input_data = sniff_data.reshape(-1, 1)
-    #normalized = scaler.fit_transform(input_data)
-    #output_data = normalized.reshape(-1)
-    normalize_factor = sniff_data[1800:1900]
-    normalize_factor = np.mean(normalize_factor)
-    output_data = sniff_data / normalize_factor
+    normalize_factor = np.max(sniff_data) * 100
+    output_data = np.divide(sniff_data, normalize_factor)
 
     return output_data
 
@@ -33,24 +33,23 @@ def bits_2_volts(bit_data, gain, carrier):
 
 
 def find_upper_bound(sniff_data, lower_bound):
-    minimum = np.min(sniff_data[lower_bound:])
-    cutoff = minimum * 1.01
+    maximum = np.max(sniff_data[lower_bound:])
+    cutoff = maximum * 0.05
     cutoff_indexes = np.where(sniff_data[lower_bound:] > cutoff)[0]
-    #print(sniff_data[lower_bound:])
     upper_bound = cutoff_indexes[-1] + lower_bound
 
     return upper_bound
 
 
 def find_lower_bound(time_stamps):
-    end_time = 2
+    end_time = 2.1
     region_indexes = np.where(time_stamps >= end_time)[0]
     lower_bound = region_indexes[0]
     return lower_bound
 
 
 def save_csv(data, file_stem):
-    column_labels = ['Odor Name', 'Odor Concentration', 'AUC']
+    column_labels = ['Odor Name', 'Odor Concentration', 'AUC', 'Time Delay']
     output = pd.DataFrame(data, columns=column_labels)
     output.to_csv(f'.\\{file_stem}-CONTAMINATION.csv', index=False)
 
@@ -75,7 +74,7 @@ def main():
     carrier_flowrate = trials['Carrier_flowrate'][type_2_trials]
     odor_name = PID_with_Plotting.decode_list(trials['odor'][type_2_trials])
 
-    for i in range(num_type_2_trials):
+    for i in tqdm.trange(num_type_2_trials):
         trial_number = type_2_trials[i]
         trial_name = trial_names[trial_number]
 
@@ -96,29 +95,24 @@ def main():
         end_baseline = int(sec_before * 1000 - 100)
         baseline = np.mean(sniff_data_array[100:end_baseline])
         sniff_data_array = sniff_data_array - baseline
+        sniff_data_array = bits_2_volts(sniff_data_array, pid_gain[i], carrier_flowrate[i])
 
         normalized_data = normalize_data(sniff_data_array)
 
-        sniff_data_array = bits_2_volts(normalized_data, pid_gain[i], carrier_flowrate[i])
-
         integral_lower_bound = find_lower_bound(time_stamp_array)
-        integral_upper_bound = find_upper_bound(sniff_data_array, integral_lower_bound)
-        print(integral_lower_bound, integral_upper_bound)
+        integral_upper_bound = find_upper_bound(normalized_data, integral_lower_bound)
 
-        y_component = sniff_data_array[integral_lower_bound:integral_upper_bound]
+        y_component = normalized_data[integral_lower_bound:integral_upper_bound]
         x_component = time_stamp_array[integral_lower_bound:integral_upper_bound]
-        fig, ax = plt.subplots()
-        #fig2,ax2 = plt.subplots()
-        #ax2.plot(time_stamp_array, sniff_data_array)
-        ax.plot(time_stamp_array, sniff_data_array)
-        ax.fill_between(x_component, y_component, color='red')
-        fig.show()
-        #fig2.show()
+
+        time_delay = integral_upper_bound - integral_lower_bound
+
         AUC = get_auc(x_component, y_component)
 
-        result = [odor_name[i], odor_concentration[i], AUC]
+        result = [odor_name[i], odor_concentration[i], AUC, time_delay]
 
         data.append(result)
+
 
     h5_file.close()
 
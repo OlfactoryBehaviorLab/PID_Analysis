8977ec682bc3910c6f32e8f9fd203b50c39352ee
olfactorybehaviorlab <dewanlab@psy.fsu.edu>
2023-05-11 09:29:15 -0400

    Code cleanup and documentation

--- DewanPID_Utils.py
+++ DewanPID_Utils.py
@@ -1,14 +1,13 @@
-import os
-
-import PySimpleGUI as sg
 import h5py
+import os
 import numpy as np
+import PySimpleGUI as sg
 
 
 def get_sniff_data(h5_file, trial_name):
-    event_data = h5_file[trial_name + '/Events']
-    sniff_data = h5_file[trial_name + '/sniff'][:]
-    #sniff_data = [each for each in sniff_data]
+    event_data = h5_file[f'{trial_name}/Events']
+    sniff_data = h5_file[f'{trial_name}/sniff'][:]
+    # Pull data out of h5 file by index
 
     return event_data, sniff_data
 
@@ -16,29 +15,36 @@ def get_sniff_data(h5_file, trial_name):
 def open_h5_file(path: str):
     try:
         h5_file = h5py.File(path, 'r')
-    except OSError or FileNotFoundError:
+    except (OSError, FileNotFoundError):
         sg.popup_error("Unable to open file. Quitting!")
         quit()
     else:
         return h5_file
+    # Try to open the h5 file, if you cannot, error out and close the program
 
 
 def decode_list(items: list):
     decoded = [item.decode('utf-8') for item in items]
     return np.array(decoded)
+    # utf-8 decode each item in list
 
 
 def condense_packets(sniff_data, sniff_samples, packet_sent_time):
     first_time_point = packet_sent_time[0] - sniff_samples[0] + 1
+    # Packet_sent_time it the time of the last packet, subtract the first sniff_sample length to get the true start time
     end_time_point = packet_sent_time[-1] + 1
+    # End point is infacte last time point, since it's the end of the last packet
     time_stamp_array = np.arange(first_time_point, end_time_point)
+    # Generate a list for the time_stamp_array,
     sniff_data_array = np.concatenate(sniff_data).ravel()
+    # Combine all the sniff_data into a single list
 
     return sniff_data_array, time_stamp_array
 
 
 def get_roi(ROI_Start: int, ROI_End: int, data_array):
     roi_index = np.where(np.logical_and(data_array > ROI_Start, data_array < ROI_End))[0]
+    # Find all indexes between two values
     return roi_index
 
 
@@ -49,6 +55,7 @@ def get_file(path=None) -> (str, str):
         filename = path
     file_stem = os.path.basename(filename)
     file_folder = os.path.dirname(filename)
+    # Get file stem to name output files; and file folder to save output files to
     return filename, file_stem, file_folder
 
 

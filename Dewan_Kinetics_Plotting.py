58937c1359d0fc2875c8925a9545dc7987f54593
DewanLab <dewanlab@psy.fsu.edu>
2023-06-12 16:10:13 -0400

    Code cleanup

--- Dewan_Kinetics_Plotting.py
+++ Dewan_Kinetics_Plotting.py
@@ -1,29 +1,21 @@
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import Dewan_PID_Utils
-from tqdm import tqdm, trange
+from tqdm import trange
 
 plt.rcParams['figure.dpi'] = 600
 
 
 def main():
-
     LINE_COLOR = 'grey'
     BOX_COLOR = 'royalblue'
     BOX_TRANSPARENCY = 0.25
-
-    file_path, file_name_stem, file_folder = Dewan_PID_Utils.get_file()
-    h5_file = Dewan_PID_Utils.open_h5_file(file_path)
-
-    num_sec_before_fv = 1
-    num_sec_after_fv = 2.5
-
     plot_sec_before_fv = 1
     plot_sec_after_fv = 6
 
-    data = []
+    file_path, file_name_stem, file_folder = Dewan_PID_Utils.get_file()
+    h5_file = Dewan_PID_Utils.open_h5_file(file_path)
 
     trials = h5_file['/Trials']
     trial_names = list(h5_file.keys())
@@ -31,14 +23,8 @@ def main():
     num_type_1_trials = len(type_1_trials)
 
     final_valve_on_time = trials['fvOnTime'][type_1_trials]
-    odor_concentration = trials['odorconc'][type_1_trials]
-    pid_pump = Dewan_PID_Utils.decode_list(trials['PIDPump'][type_1_trials])
-    pid_spacer = trials['PIDSpace'][type_1_trials]
     pid_gain = trials['PIDGain'][type_1_trials]
-    odor_vial = trials['odorvial'][type_1_trials]
     carrier_flowrate = trials['Carrier_flowrate'][type_1_trials]
-    dilutor_flowrate = trials['Dilutor_flowrate'][type_1_trials]
-    odor_name = Dewan_PID_Utils.decode_list(trials['odor'][type_1_trials])
 
     fig, ax1 = plt.subplots()
     y_vals = []
@@ -61,7 +47,6 @@ def main():
 
         TOI_start_plot = final_valve_on_time[i] - plot_sec_before_fv * 1000
         TOI_end_plot = final_valve_on_time[i] + plot_sec_after_fv * 1000
-
 
         plot_roi_index = Dewan_PID_Utils.get_roi(TOI_start_plot, TOI_end_plot, time_stamp_array_plot)
         sniff_data_array_plot = sniff_data_array_plot[plot_roi_index]
@@ -88,13 +73,14 @@ def main():
     ax1.get_yaxis().set_visible(False)
     rect = patches.Rectangle((0, 0), 2, (max(y_vals)*1.05), color=BOX_COLOR, alpha=BOX_TRANSPARENCY)
     ax1.add_patch(rect)
-    ax1.set_xlabel("Time (s)", fontfamily='arial', fontsize=12, fontweight= 'bold')
+    ax1.set_xlabel("Time (s)", fontfamily='arial', fontsize=12, fontweight='bold')
     x_ticks = np.arange(-1, 5)
 
     plt.xticks(x_ticks)
 
     Dewan_PID_Utils.save_figure(file_name_stem, file_folder, fig)
     fig.show()
+
 
 if __name__ == "__main__":
     main()

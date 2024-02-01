227064ff668013c955e4ab97f0508766d95ea33a
Austin Pauley <austin2118ace@gmail.com>
2024-02-01 16:36:25 -0500

    New function to parse out the trial settings from the MATLAB mat file

--- Utils/Dewan_MAT_Parser.py
+++ Utils/Dewan_MAT_Parser.py
@@ -1,11 +1,13 @@
 import pathlib
 import scipy.io as sio
+import pandas as pd
+# import numpy as np
 
-
-def parse_mat(path: pathlib.Path) -> dict:
+def parse_mat(path: pathlib.Path) -> pd.DataFrame:
     mat_file = load_mat(path)
 
-
+    session_data = mat_file['SessionData'][0]
+    trial_settings = parse_settings(session_data)
 
 def load_mat(path: pathlib.Path) -> dict:
     mat_file = []
@@ -20,3 +22,13 @@ def load_mat(path: pathlib.Path) -> dict:
         return mat_file
 
     return mat_file
+
+
+def parse_settings(session_data) -> pd.DataFrame:
+    settings = session_data['Settings'][0][0]  # Grab settings list that is deeply nested
+    settings = pd.DataFrame(settings)  # Convert to dataframe
+    settings = settings.map(lambda x: x[0][0])
+    # Each data point is wrapped in a double array [[data]]; lambda function applied to each data point where the
+    # double array is removed
+    return settings
+

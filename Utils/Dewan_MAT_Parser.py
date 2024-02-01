9f370a2bdc3525444c15a5bfeb8e551a35d3d3b7
Austin Pauley <austin2118ace@gmail.com>
2024-02-01 16:50:31 -0500

    Parse session_information from Bpod; function to collapse the weird nested arrays that the mat files sometimes have

--- Utils/Dewan_MAT_Parser.py
+++ Utils/Dewan_MAT_Parser.py
@@ -1,13 +1,14 @@
 import pathlib
 import scipy.io as sio
 import pandas as pd
-# import numpy as np
+import numpy as np
 
-def parse_mat(path: pathlib.Path) -> pd.DataFrame:
+def parse_mat(path: pathlib.Path):
     mat_file = load_mat(path)
 
     session_data = mat_file['SessionData'][0]
     trial_settings = parse_settings(session_data)
+
 
 def load_mat(path: pathlib.Path) -> dict:
     mat_file = []
@@ -32,3 +33,26 @@ def parse_settings(session_data) -> pd.DataFrame:
     # double array is removed
     return settings
 
+
+def session_info(session_data) -> pd.DataFrame:
+    info = session_data['Info'][0][0]
+    info = pd.DataFrame(info)
+    info = info.map(np.ravel)
+    info = info.map(lambda x: x[0])
+
+    firmware = info['Firmware']
+    firmware = collapse_array_as_str(firmware)
+    info['Firmware'] = firmware
+
+    circuit_rev = info['CircuitRevision']
+    circuit_rev = collapse_array_as_str(circuit_rev)
+    info['CircuitRevision'] = circuit_rev
+
+    return info
+
+
+def collapse_array_as_str(array):
+    array = array.apply(np.hstack).apply(np.ravel)[0]
+    array = '.'.join(list(array.astype(str)))
+
+    return array
